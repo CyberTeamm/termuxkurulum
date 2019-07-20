de9f285b1dc1fb392b41c4b68faac3cd3c8772dd
CyberTeamm <52137281+CyberTeamm@users.noreply.github.com>
2019-07-20 18:32:50 +0300

    Add files via upload

---
+++ ccteam.sh
@@ -0,0 +1,12 @@
+clear
+echo -e "\e[32mKurulum Başlıyor...\e[0m"
+
+pkg install python
+pkg install python2
+pkg install python3
+pkg install figlet
+pkg install git
+pkg install php
+pkg install wget
+
+echo -e "\e[32mKurulum Tamamlandı\e[0m"
